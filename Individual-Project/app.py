10b01da5d9fb2a0f11764ff42470410a1c234a8f
royba23-meet <roybacharschool@gmail.com>
2022-08-04 14:26:29 +0300

    Done

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from datetime import datetime
 import pyrebase
 
-
 CONFIG = { 
   "apiKey": "AIzaSyC991VhI6UMnqBc9qF_-KX-77vN1QQWGpM",
   "authDomain": "individual-project-2c4bc.firebaseapp.com",
@@ -14,7 +13,6 @@ CONFIG = {
   "messagingSenderId": "755275024194",
   "appId": "1:755275024194:web:f5fb175cf7d1e3c0a0eadb",
   "measurementId": "G-4GBX17PQQ1",
-  "databaseURL": "https://individual-project-2c4bc-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 
@@ -52,11 +50,10 @@ def signin():
         user = {'email': request.form['email'], 'password': request.form['password']}
         try:
             session['user'] = auth.sign_in_with_email_and_password(user['email'], user['password'])
-            session['username'] = db.child('users').child(session['user']['localId']).get().val()['username']
             return redirect(url_for('sock'))
         except Exception:
             flash('Invalid username or password')
-            return redirect(url_for('signin'))
+            return redirect(url_for('signin', error=True))
     return render_template('signin.html')
 
 
@@ -65,7 +62,8 @@ def sock():
     reviews = db.child('reviews').child('socks').get().val().values()
     if request.method == 'POST':
         try:
-            sock = {'name': request.form['name'], 'username':session['user']['username'],'review':request.form['review'],'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'data':{}}
+            session['username'] = db.child('users').child(session['user']['localId']).get().val()['username']
+            sock = {'name': request.form['name'], 'username':session['username'],'review':request.form['review'],'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
             db.child('reviews').child('socks').push(sock)
             reviews = db.child('reviews').child('socks').get().val().values()
             return redirect(url_for('sock', reviews=reviews))
@@ -73,6 +71,13 @@ def sock():
             flash('Invalid file')
             return redirect(url_for('sock', reviews=reviews))
     return render_template('sock.html', reviews=reviews)
+
+
+@app.route('/dictator', methods=['GET', 'POST'])
+def dictator():
+    if auth.current_user is None:
+        return redirect(url_for('signin'))
+    
 
 
 if __name__ == '__main__':
